8c69352b1fb97592055a0dfb8a91d9d16ef44d26
OlgaLeshchinskaya <reserv2022post@gmail.com>
2022-10-14 12:31:01 +0300

    feat: solution task carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let time = 0;
+
+  if(typeof sampleActivity !== 'string' || sampleActivity <= 0 || sampleActivity === null || sampleActivity === undefined || sampleActivity > MODERN_ACTIVITY || isNaN(parseInt(sampleActivity))){
+    return false
+  }else {
+    time = (Math.log((MODERN_ACTIVITY / +sampleActivity))) / (0.693 / HALF_LIFE_PERIOD);
+   
+  }
+  return Math.ceil(time);
+
 }
 
 module.exports = {
